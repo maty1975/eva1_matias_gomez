e65dbb4ddec919616ae935b73d8feb70eeda0a0b
maty1975 <matiasgomezaranda@gmail.com>
2025-04-06 21:28:08 -0400

    ahora tengo conectado el js al html

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var correito = document.querySelector(".correo");
+correito.innerHTML = "noobmaster96@gmail.com"
+
+
+
+var telefono = document.querySelector(".numero");
+telefono.innerHTML ="+ 56 9 1234 5678"
