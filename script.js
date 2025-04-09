5a0a9debc6366bd14918b64ac931e03215856b0b
maty1975 <matiasgomezaranda@gmail.com>
2025-04-08 23:41:39 -0400

    3 cambios de paginas funcionando

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
 
 var correito = document.querySelector(".correo");
-correito.innerHTML = "noobmaster96@gmail.com"
+if  (correito) correito.innerHTML = "noobmaster96@gmail.com"
 
+var s_juegos = document.querySelector(".s_proyectos")
+if(s_juegos) s_juegos.innerHTML = "Proyectos"
+
+var s_quiensoy = document.querySelector(".s_quien")
+if(s_quiensoy) s_quiensoy.innerHTML ="quien soy"
+
+var s_arte = document.querySelector(".s_arte")
+if(s_arte) s_arte.innerHTML = "Arte"
 
 
 var telefono = document.querySelector(".numero");
-telefono.innerHTML ="+ 56 9 1234 5678"
+if(telefono) telefono.innerHTML ="+ 56 9 1234 5678"
